2354e2d567cadfd83de4d50ad9f0245f6a2ea033
Daniel Martuszewski <daniel10a@o2.pl>
2011-05-23 21:30:31 +0200

    Start commit. Class skel.

---
+++ ftp_client.php
@@ -0,0 +1,16 @@
+<?php
+
+
+/**
+ * FTPClient class is a imlementation of FTP protocol
+ * for simple use in PHP scripts
+ * 
+ * @author Daniel Martuszewski <daniel10a at o2.pl>
+ * @filesource ftp_client.php
+ * 
+ */
+class FTPClient {
+    
+}
+
+?>
