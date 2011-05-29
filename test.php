ca82f65d8e07ab1df421561d799fbd27f68d58d1
Daniel Martuszewski <daniel10a@o2.pl>
2011-05-29 09:26:23 +0200

    Add test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <title>NewPHPFile</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div id="page">
+            <?php
+            ?>
+        </div>
+    </body>
+</html>
