3461ed5575c95a9d79ba763f1191eea009fa2fb2
Daniel Martuszewski <daniel10a@o2.pl>
2011-06-02 06:17:50 +0200

    Add upload and make dir functionality

--- ftp_client.php
+++ ftp_client.php
@@ -75,6 +75,16 @@ class FTPClient {
      * @var resource
      */
     private $connectionHandler;
+    /**
+     * Files to upload in format $files['remoteFile'] => 'localFile'
+     * @var array 
+     */
+    private $files = array();
+    /**
+     * Extensions of ASCII files
+     * @var array 
+     */
+    private $asciiExtensions = array('txt', 'csv');
 
 
     public function __construct() {
@@ -96,12 +106,10 @@ class FTPClient {
         // $match[3] = sld.domain.tld
         // $match[4] = /path1/path2/
         preg_match('/ftp:\/\/(.*?):(.*?)@(.*?)(\/.*)/i', $uri, $match);
-        echo '<pre>';
-        print_r($match);
         $this->username = $match[1];
         $this->password = $match[2];
-        $this->host     = $match[3];
-        $this->path     = $match[4];
+        $this->host = $match[3];
+        $this->path = $match[4];
     }
 
 
@@ -122,33 +130,35 @@ class FTPClient {
         return $this->messages[count($this->messages) - 1];
     }
 
+
     /**
      * Connect and login to the FTP server.
      * @return bool
      */
     public function connect() {
-        if ($this->secure) {
+        if( $this->secure ) {
             $this->connectionHandler = ftp_ssl_connect($this->host, $this->port, $this->timeout);
         } else {
             $this->connectionHandler = ftp_connect($this->host, $this->port, $this->timeout);
         }
-        
+
         $loginRes = ftp_login($this->connectionHandler, $this->username, $this->password);
-        
-        if( (!$this->connectionHandler) || (!$loginRes)) {
+
+        if( (!$this->connectionHandler) || (!$loginRes) ) {
             $this->logMessage('FTP connection has failed!');
             $this->logMessage(sprintf('Attempted to connect to %s for user %s', $this->host, $this->username), true);
-            
+
             return false;
         } else {
             $this->logMessage(sprintf('Connected to %s for user %s', $this->host, $this->username));
             $this->loginOk = true;
         }
-        
+
         ftp_chdir($this->connectionHandler, $this->path);
-        
+
         return true;
     }
+
 
     /**
      * Log all messages into property
@@ -157,10 +167,11 @@ class FTPClient {
      */
     private function logMessage($message, $isError = null) {
         $this->messages[] = $message;
-        if ($isError) {
+        if( $isError ) {
             $this->isError = true;
         }
     }
+
 
     /**
      * Show if error was occured
@@ -177,12 +188,101 @@ class FTPClient {
      * @param bool $recursive optional
      * @return array
      */
-    public function getFiles($directory = null, $recursive = true) {
-        if($directory) {
+    public function ls($directory = null, $recursive = true) {
+        if( $directory ) {
             $this->path = $directory;
         }
         return ftp_rawlist($this->connectionHandler, $this->path, $recursive);
     }
+
+
+    /**
+     * Add file to stack of files to upload
+     * @param string $localFile
+     * @param string $remoteFile
+     */
+    public function addFile($localFile, $remoteFile = null) {
+        if( !file_exists($localFile) ) {
+            $this->logMessage(sprintf('File %s does not exist.', $localFile), true);
+
+            return false;
+        }
+        if( !$remoteFile ) {
+            $remoteFile = basename($localFile);
+        }
+        $this->files[$remoteFile] = $localFile;
+    }
+
+
+    /**
+     * Set ASCII files extensions 
+     * (use to determinate filetype during upload)
+     * @param array $extensions 
+     */
+    public function setAsciiExtensions($extensions) {
+        $this->asciiExtensions = $extensions;
+    }
+
+
+    /**
+     * Add extension to ASCII file extensions
+     * (use to determinate filetype during upload)
+     * @param string $extension 
+     */
+    public function addAsciiExtension($extension) {
+        $this->asciiExtensions[] = $extension;
+    }
+
+
+    /**
+     * Determinate if file is ASCII file. Based on
+     * filename extension and $asciiExtensions property.
+     * @param string $filename
+     * @return bool 
+     */
+    private function isAsciiFile($filename) {
+        if( in_array(array_pop(explode('.', $filename)), $this->asciiExtensions) ) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Upload files from $files property
+     */
+    public function upload() {
+        foreach( $this->files as $remote => $local ) {
+            if( $this->isAsciiFile($local) ) {
+                $mode = FTP_ASCII;
+            } else {
+                $mode = FTP_BINARY;
+            }
+            if( ftp_put($this->connectionHandler, $remote, $local, $mode) ) {
+                $this->logMessage(sprintf('File %s uploaded as %s', $local, $remote));
+            } else {
+                $this->logMessage(sprintf('Failed uploading file "%s"!', $local), true);
+            }
+        }
+    }
+
+
+    /**
+     * Make dir $directory on FTP 
+     * @param string $directory
+     * @return bool 
+     */
+    public function mkdir($directory) {
+        if( ftp_mkdir($this->connectionHandler, $directory) ) {
+            $this->logMessage(sprintf('Directory %s created', $directory));
+
+            return true;
+        }
+        $this->logMessage(sprintf('Failed creating directory "%s"!', $directory));
+
+        return false;
+    }
+
 }
 
 ?>
